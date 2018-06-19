8cfd2bd9840a2fc3a2d8809afa55ff817b5760d8
jdmurphy <jdmurphy@iastate.edu>
2018-06-19 16:04:55 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs'); // Handlebars.js
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = now + ":" + " " + req.method + " " + req.url;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to the server.log file...');
+    }
+  });
+  next();
+});
+
+
+//Maintenance Middleware
+/*
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+*/
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('scream', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome!!!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "An Error occured while handling your request :("
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server Started on Port 3000');
+});
